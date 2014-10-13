cdb03989def648ba87e47fff5d459580183a25a6
snehapvs <snehapvs477@gmail.com>
2014-10-13 11:32:51 +0530

    FX version

---
+++ della_FX/src/application/Login_controller.java
@@ -0,0 +1,34 @@
+package application;
+
+import java.net.URL;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.ResourceBundle;
+
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
+import javafx.event.ActionEvent;
+import javafx.event.EventHandler;
+import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+import javafx.scene.control.*;
+
+public class Login_controller implements Initializable {
+
+	@Override
+	public void initialize(URL location, ResourceBundle resources) {
+		// TODO Auto-generated method stub
+
+	}
+
+	public TextField username;
+
+	public TextField password;
+	public Button login;
+	void performlogin(){
+		
+		
+		
+	}
+
+}
