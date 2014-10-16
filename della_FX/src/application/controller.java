fbd620b70bccc95a4f5970583efe8c4fffa6e661
vaibhavmsit2013 <vaibhav171289@gmail.com>
2014-10-16 10:21:54 +0530

    button disabling
    a new function added to this class which is used for disabling the
    button

--- della_FX/src/application/controller.java
+++ della_FX/src/application/controller.java
@@ -4,15 +4,23 @@ import java.net.URL;
 import java.util.ResourceBundle;
 
 import javafx.fxml.Initializable;
+import javafx.scene.control.Button;
 
 public class controller implements Initializable
 {
 
+	private Button update_action;
 	@Override
 	public void initialize(URL arg0, ResourceBundle arg1) 
 	{
-		// TODO Auto-generated method stub
+
+		disable();
 		
 	}
 
+	public void disable()
+	{
+		update_action.setVisible(false);
+	}
+	
 }
