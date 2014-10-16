56c1cc1683353d2351a1c7dd226ff4ad609e72db
vaibhavmsit2013 <vaibhav171289@gmail.com>
2014-10-16 12:07:23 +0530

    changing the
    della

--- della_FX/src/application/controller.java
+++ della_FX/src/application/controller.java
@@ -4,23 +4,17 @@ import java.net.URL;
 import java.util.ResourceBundle;
 
 import javafx.fxml.Initializable;
-import javafx.scene.control.Button;
 
 public class controller implements Initializable
 {
 
-	private Button update_action;
+
 	@Override
 	public void initialize(URL arg0, ResourceBundle arg1) 
 	{
 
-		disable();
 		
-	}
-
-	public void disable()
-	{
-		update_action.setVisible(false);
+		
 	}
 	
 }
