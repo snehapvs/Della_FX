5b44fc9eb87ae120c967777578f20f8040e840c6
vaibhavmsit2013 <vaibhav171289@gmail.com>
2014-10-20 12:13:34 +0530

    added names to button and quit button
    a query in quit button as well as names for new button in java fxml file

--- della_FX/src/application/controller.java
+++ della_FX/src/application/controller.java
@@ -1,11 +1,13 @@
 package application;
 
 import java.net.URL;
+import java.sql.SQLException;
+import java.sql.Statement;
 import java.util.ResourceBundle;
 
-import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
 import javafx.scene.control.Button;
+import javafx.stage.Stage;
 
 public class controller implements Initializable
 {
@@ -17,11 +19,39 @@ public class controller implements Initializable
 	}
 	
 	
-	public Button button_action;
-
+	public Button update_actionItem;
+	public Button quit;
+	public Button clear_actionItem;
+	public Button create_actionItem;
+	public Button delete_actionItem;
+	
 	public  void disable()
 	{
 		System.out.println("in disable function");
-		button_action.setDisable(true);
+		update_actionItem.setDisable(true);
+		clear_actionItem.setDisable(true);
+		create_actionItem.setDisable(true);
+		delete_actionItem.setDisable(true);
+	}
+	public void update_actionItem(){}
+	public void clearForm_actionItem()
+	{}
+	public void createNew_actionItem()
+	{}
+	public void deleteItem_actionItem()
+	{}
+	public void quitWindow()
+	{
+		Statement stmt= Main.getStmt();
+		try {
+			int row=stmt.executeUpdate("UPDATE enhanced_della.userlog SET lockstatus=0 WHERE lockstatus =1;");
+			if(row!=0)
+				System.out.println("Successful updation into userlog");
+			Stage st= (Stage)quit.getScene().getWindow();
+			st.close();
+		} catch (SQLException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
 	}
 }
